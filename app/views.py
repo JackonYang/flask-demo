cf1337a316631c299bca9aeefe9f776b1cf9399d
Jackon <jiekunyang@gmail.com>
2015-11-29 00:27:13 +0800

    use templates

--- app/views.py
+++ app/views.py
@@ -1,14 +1,14 @@
 # -*- Encoding: utf-8 -*-
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 bp = Blueprint('default', __name__)
 
 
 @bp.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('default/index.html')
 
 
 @bp.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return render_template('default/user.html', name=name)
