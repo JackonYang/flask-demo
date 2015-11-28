6f0b9a7b9e51caceb79321e06fe87019608412ff
Jackon <jiekunyang@gmail.com>
2015-11-29 01:31:14 +0800

    forms with POST method

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,10 @@ from flask import Flask
 
 def make_app():
     app = Flask(__name__)
+
+    # used by flask-wtf for CSRF protection
+    app.config['SECRET_KEY'] = 'hard to guess string'
+
     registe_routes(app)
     return app
 
--- app/views.py
+++ app/views.py
@@ -1,12 +1,19 @@
 # -*- Encoding: utf-8 -*-
 from flask import Blueprint, render_template
+from .forms import NameForm
 
 bp = Blueprint('default', __name__)
 
 
-@bp.route('/')
+@bp.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('default/index.html')
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('default/index.html',
+                           form=form, name=name)
 
 
 @bp.route('/user/<name>')
